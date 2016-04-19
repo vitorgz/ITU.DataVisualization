932c81ca14c1c9eefe1bf93f26fbcc2c48e598c8
vitor_gz <dominguez.vitor@gmail.com>
2016-04-19 18:54:51 +0200

    UI refactoring (first step)

--- ITU.Ckan.DataVisualization.Web/Controllers/DataSourceController.cs
+++ ITU.Ckan.DataVisualization.Web/Controllers/DataSourceController.cs
@@ -20,7 +20,6 @@ namespace ITU.Ckan.DataVisualization.Web.Controllers
 
             List<SelectListItem> items = new List<SelectListItem>();
             items.Add(new SelectListItem { Text = "http://data.kk.dk/", Value = "http://data.kk.dk/" });
-            items.Add(new SelectListItem { Text = "http://data.kk.dk/", Value = "http://data.kk.dk/" });
             items.Add(new SelectListItem { Text = "http://datahub.io", Value = "http://datahub.io/" });
             items.Add(new SelectListItem { Text = "http://data.amsterdam.nl/", Value = "http://data.amsterdam.nl/", Selected = true });
             items.Add(new SelectListItem { Text = "Other", Value = "3" });
@@ -40,7 +39,7 @@ namespace ITU.Ckan.DataVisualization.Web.Controllers
             var pck = await new SourceFactory().Initialize().GetPackages(id);
 
             var vis = RootInstance.Current.GetVisualization("test");
-            var source = vis.GetSourceById(x => x.name == "http://data.kk.dk/");
+            var source = vis.GetSourceById(x => x.name == id);
             source.packages = pck.Create().packages;
 
             List<SelectListItem> items = new List<SelectListItem>();
@@ -54,15 +53,15 @@ namespace ITU.Ckan.DataVisualization.Web.Controllers
             return Json(new SelectList(items, "Value", "Text"));
         }
 
-        public async Task<JsonResult> GetDataSets(string id)
+        public async Task<JsonResult> GetDataSets(string src, string pck)
         {
             var vis = RootInstance.Current.GetVisualization("test");
-            var source = vis.GetSourceById(x => x.name == "http://data.kk.dk/");            
+            var source = vis.GetSourceById(x => x.name == src);            
 
-            var ds = await new PackageFactory().Initialize().GetDataSetsById(source.name, id);
+            var ds = await new PackageFactory().Initialize().GetDataSetsById(source.name, pck);
             var newPkg = ds.Create();
 
-            var pkg = source.packages.Where(x => x.name == id).FirstOrDefault();
+            var pkg = source.packages.Where(x => x.name == pck).FirstOrDefault();
             source.packages.ToList().ForEach(x => x.selected = false);
             pkg.selected = true;
             pkg.dataSets = newPkg.dataSets;
@@ -73,6 +72,24 @@ namespace ITU.Ckan.DataVisualization.Web.Controllers
             {
                 items.Add(new SelectListItem { Text = item.name, Value = item.name });
             }
+
+            return Json(items);
+        }
+
+        public async Task<JsonResult> GetFields(string src, string pck, string dts)
+        {
+            var visual = RootInstance.Current.GetVisualization("test");
+            var source = visual.GetSourceById(x => x.name == src);
+            var ds = source.packages.Where(x => x.name == pck).FirstOrDefault().dataSets;
+            var fields = ds.Where(x=>x.name == dts).Where(x => x.format == "CSV").FirstOrDefault().fields;
+
+            List<SelectListItem> items = new List<SelectListItem>();
+            foreach (var item in fields)
+            {
+                items.Add(new SelectListItem { Text = (item.id + "- (" + item.type + ")"), Value = item.id.ToString() });
+            }
+
+            //ViewData["fields"] = items;
 
             return Json(items);
         }
@@ -90,9 +107,26 @@ namespace ITU.Ckan.DataVisualization.Web.Controllers
             return Json(true);
         }
 
+        //TODO set select flag!
+        public void GetDataForChart(string idX, string idY)
+        {
+            var visual = RootInstance.Current.GetVisualization("test");
+            var source = visual.GetSourceById(x => x.name == "http://data.kk.dk/");
+            var ds = source.packages.Where(x => x.selected).FirstOrDefault().dataSets;
+            var fields = ds.Where(x => x.format == "CSV").FirstOrDefault().fields;
+
+            var select = fields.Where(x => x.id.ToString() == idX || x.id.ToString() == idY).ToList();
+            var nonSelect = fields.Where(x => x.id.ToString() != idX || x.id.ToString() != idY).ToList();
+            nonSelect.ForEach(x => x.selected = false);
+            select.ForEach(x => x.selected = true);
+            select.Where(x => x.id.ToString() == idX).FirstOrDefault().xAxys = true;
+
+        }
+
+
         public ActionResult ChartRedirect()
         {
-            return RedirectToAction("Index", "Chart");
+            return RedirectToAction("Index", "Draw");
         }
     }
 }
