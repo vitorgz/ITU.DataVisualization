d951c786bd614607137ef97776ce49da8c116bbb
vitor_gz <dominguez.vitor@gmail.com>
2016-04-26 18:00:54 +0200

    restful api handling exceptions

--- ITU.Ckan.DataVisualization.CloudApi/Ckan/CkanController.cs
+++ ITU.Ckan.DataVisualization.CloudApi/Ckan/CkanController.cs
@@ -18,97 +18,134 @@ namespace ITU.Ckan.DataVisualization.CloudApi.Ckan
         [HttpPost]
         public async Task<HttpResponseMessage> GetPackages(SourceDTO source)
         {
-            //http://data.kk.dk/api/action/package_list
-            var response = await GenericApi.GenericRestfulClient.Get<ListDeserialize>(source.sourceName, source.command);
+            try
+            {
+                //http://data.kk.dk/api/action/package_list
+                var response = await GenericApi.GenericRestfulClient.Get<ListDeserialize>(source.sourceName, source.command);
 
-            var pkgs = CloudApiHelpers.ConvertToListOfType<Package>(response);
+                var pkgs = CloudApiHelpers.ConvertToListOfType<Package>(response);
 
-            return Request.CreateResponse(HttpStatusCode.OK, pkgs);
+                return Request.CreateResponse(HttpStatusCode.OK, pkgs);
+            }
+            catch (HttpRequestException ex)
+            {
+                //throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.InternalServerError));
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
         }
 
         [Route("api/GetDataSet")]
         [HttpPost]
         public async Task<HttpResponseMessage> GetDataSet(Source source)
         {
-            var pkg = new Package();
-            pkg.name = source.packages.FirstOrDefault().name;
-            pkg.dataSets = new List<DataSet>();
+            try {
+                var pkg = new Package();
+                pkg.name = source.packages.FirstOrDefault().name;
+                pkg.dataSets = new List<DataSet>();
 
-            //http://data.kk.dk/api/action/package_show?id=aalegraes
-            var response = await GenericApi.GenericRestfulClient.Get<PackageDeserialize>(source.name, "/api/action/package_show", source.packages.FirstOrDefault().name);
+                //http://data.kk.dk/api/action/package_show?id=aalegraes
+                var response = await GenericApi.GenericRestfulClient.Get<PackageDeserialize>(source.name, "/api/action/package_show", source.packages.FirstOrDefault().name);
 
-            //find CSV datasets
-            var csvFiles = response.result.resources.Where(x => x.format == "CSV");
+                //find CSV datasets
+                var csvFiles = response.result.resources.Where(x => x.format == "CSV");
 
-            //check if it's a DataStore and if it's, get MetaData
-            foreach (var file in csvFiles)
-            {
-                var resp = await GenericApi.GenericRestfulClient.Get<DataSetDeserialize>(source.name, "/api/action/datastore_search", file.id, 1);
-                file.fields = resp.result.fields;
-                
+                //check if it's a DataStore and if it's, get MetaData
+                foreach (var file in csvFiles)
+                {
+                    var resp = await GenericApi.GenericRestfulClient.Get<DataSetDeserialize>(source.name, "/api/action/datastore_search", file.id, 1);
+                    file.fields = resp.result.fields;
+
+                }
+
+                //add data sets to package
+                response.result.resources.ForEach(x => (pkg.dataSets as List<DataSet>).Add(x));
+
+                return Request.CreateResponse(HttpStatusCode.OK, pkg);
             }
-            
-            //add data sets to package
-            response.result.resources.ForEach(x => (pkg.dataSets as List<DataSet>).Add(x));
-
-            return Request.CreateResponse(HttpStatusCode.OK, pkg);
+            catch (HttpRequestException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
         }
 
         [Route("api/GetMetaData")]
         [HttpPost]
         public async Task<HttpResponseMessage> GetMetaData(string url, string dataSourceId)
         {
-            //http://data.kk.dk/api/action/datastore_search?resource_id=123014980123948702&limit=1
-            var response = await GenericApi.GenericRestfulClient.Get<DataSetDeserialize>(url, "/api/action/datastore_search", dataSourceId, 1);
-            var fields = response.result.fields;
+            try
+            {
+                //http://data.kk.dk/api/action/datastore_search?resource_id=123014980123948702&limit=1
+                var response = await GenericApi.GenericRestfulClient.Get<DataSetDeserialize>(url, "/api/action/datastore_search", dataSourceId, 1);
+                var fields = response.result.fields;
 
-            return Request.CreateResponse(HttpStatusCode.OK, fields);
+                return Request.CreateResponse(HttpStatusCode.OK, fields);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
         }
 
         [Route("api/GetDataLimit")]
         [HttpPost]
         public async Task<HttpResponseMessage> GetDataLimit(SourceDTO source)
         {
-            //http://data.kk.dk/api/action/datastore_search?resource_id=123014980123948702&limit=1
-            var response = await GenericApi.GenericRestfulClient.Get(
-                source.sourceName, 
-                "/api/action/datastore_search", 
-                source.dataSetId, 
-                source.limit);
+            try
+            {
+                //http://data.kk.dk/api/action/datastore_search?resource_id=123014980123948702&limit=1
+                var response = await GenericApi.GenericRestfulClient.Get(
+                    source.sourceName,
+                    "/api/action/datastore_search",
+                    source.dataSetId,
+                    source.limit);
 
-            //TODO parse response -> read first Fields, and them map to formatted object
+                //TODO parse response -> read first Fields, and them map to formatted object
 
-            return Request.CreateResponse(HttpStatusCode.OK, response);
-        }
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+}
 
         [Route("api/GetDataLimitOffset")]
         [HttpPost]
         public async Task<HttpResponseMessage> GetDataLimitOffset(SourceDTO source)
         {
-            //http://data.kk.dk/api/action/datastore_search?resource_id=123014980123948702&limit=1
-            var response = await GenericApi.GenericRestfulClient.Get(
-                source.sourceName,
-                "/api/action/datastore_search",
-                source.dataSetId, 
-                source.limit, 
-                source.offset);
+            try
+            {
+                //http://data.kk.dk/api/action/datastore_search?resource_id=123014980123948702&limit=1
+                var response = await GenericApi.GenericRestfulClient.Get(
+                    source.sourceName,
+                    "/api/action/datastore_search",
+                    source.dataSetId,
+                    source.limit,
+                    source.offset);
 
-            //TODO parse response -> read first Fields, and them map to formatted object
+                //TODO parse response -> read first Fields, and them map to formatted object
 
-            return Request.CreateResponse(HttpStatusCode.OK, response);
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
         }
 
         [Route("api/GetData")]
         [HttpPost]
         public async Task<HttpResponseMessage> GetData(VisualDTO visual)
         {
-            //http://data.kk.dk/api/action/datastore_search?resource_id=123014980123948702
-
-            // var dict = new Dictionary<string, List<string>>();
-
-            foreach (var item in visual.sources)
+            try
             {
-                var flds = item.fields.Select(x => x.id.ToString()).ToList();
+                //http://data.kk.dk/api/action/datastore_search?resource_id=123014980123948702
+
+                // var dict = new Dictionary<string, List<string>>();
+
+                foreach (var item in visual.sources)
+                {
+                    var flds = item.fields.Select(x => x.id.ToString()).ToList();
                     var values = new Tuple<string, List<string>>(item.dataSetId, flds);
 
                     //for each data source
@@ -117,81 +154,120 @@ namespace ITU.Ckan.DataVisualization.CloudApi.Ckan
 
                     //TODO convert fileds.Types to our types???
                     item.fields.ForEach(x => x.type = CloudApiHelpers.ResolveType(x.type));
-                   CloudApiHelpers.ProcessJsonResponse(response, item.fields);
+                    CloudApiHelpers.ProcessJsonResponse(response, item.fields);
+                }
+
+                //Merge all Data in one DAtaTable
+                var table = CloudApiHelpers.CreateDataTable(visual);
+
+
+                //create RowData
+                //CloudApiHelpers.MergeData(visual);
+
+                return Request.CreateResponse(HttpStatusCode.OK, table);
             }
-
-            //Merge all Data in one DAtaTable
-            var table = CloudApiHelpers.CreateDataTable(visual);
-
-
-            //create RowData
-            //CloudApiHelpers.MergeData(visual);
-
-
-            return Request.CreateResponse(HttpStatusCode.OK, table);
+            catch (HttpRequestException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
         }
 
         [Route("api/GetDataPieChart")]
         [HttpPost]
         public async Task<HttpResponseMessage> GetDataPieChart(VisualDTO visual)
         {
-            var source = visual.sources.FirstOrDefault();
-            var xAxys = source.fields.FirstOrDefault();
+            try
+            {
+                var source = visual.sources.FirstOrDefault();
+                var xAxys = source.fields.FirstOrDefault();
 
-            var values = new Tuple<string, List<string>>(source.dataSetId, new List<string>() { xAxys.name});
+                var values = new Tuple<string, List<string>>(source.dataSetId, new List<string>() { xAxys.name });
 
-            var response = await GenericApi.GenericRestfulClient.
-                        Get<object>(source.sourceName, "/api/action/datastore_search_sql?sql=", xAxys.name);
+                var response = await GenericApi.GenericRestfulClient.
+                            Get<object>(source.sourceName, "/api/action/datastore_search_sql?sql=", xAxys.name);
 
-            var fieldsList = new List<Field>() { xAxys };
-            source.fields = fieldsList;
-            CloudApiHelpers.ProcessJsonResponse(response, fieldsList);
+                var fieldsList = new List<Field>() { xAxys };
+                source.fields = fieldsList;
+                CloudApiHelpers.ProcessJsonResponse(response, fieldsList);
 
-            var table = CloudApiHelpers.PieChartAnalizeAndCreateTable(source.fields.FirstOrDefault().record);
+                var table = CloudApiHelpers.PieChartAnalizeAndCreateTable(source.fields.FirstOrDefault().record);
 
-            return Request.CreateResponse(HttpStatusCode.OK, table);
+                return Request.CreateResponse(HttpStatusCode.OK, table);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
         }
 
         [Route("api/GetTags")]
         [HttpPost]
         public async Task<HttpResponseMessage> GetTags(SourceDTO source)
         {
-            var response = await GenericApi.GenericRestfulClient.Get<ListDeserialize>(source.sourceName, source.command);
+            try
+            {
+                var response = await GenericApi.GenericRestfulClient.Get<ListDeserialize>(source.sourceName, source.command);
 
-            var tags = CloudApiHelpers.ConvertToListOfType<Tag>(response);
+                var tags = CloudApiHelpers.ConvertToListOfType<Tag>(response);
 
-            return Request.CreateResponse(HttpStatusCode.OK, tags);
+                return Request.CreateResponse(HttpStatusCode.OK, tags);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
         }
 
         [Route("api/GetGroups")]
         [HttpPost]
         public async Task<HttpResponseMessage> GetGroups(SourceDTO source)
         {
-            var response = await GenericApi.GenericRestfulClient.Get<ListDeserialize>(source.sourceName, source.command);
+            try
+            {
+                var response = await GenericApi.GenericRestfulClient.Get<ListDeserialize>(source.sourceName, source.command);
 
-            var groups = CloudApiHelpers.ConvertToListOfType<Group>(response);
+                var groups = CloudApiHelpers.ConvertToListOfType<Group>(response);
 
-            return Request.CreateResponse(HttpStatusCode.OK, groups);
+                return Request.CreateResponse(HttpStatusCode.OK, groups);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
         }
 
         [Route("api/GetOrganizations")]
         [HttpPost]
         public async Task<HttpResponseMessage> GetOrganizations(SourceDTO source)
         {
-            var response = await GenericApi.GenericRestfulClient.Get<ListDeserialize>(source.sourceName, source.command);
+            try
+            {
+                var response = await GenericApi.GenericRestfulClient.Get<ListDeserialize>(source.sourceName, source.command);
 
-            var orgs = CloudApiHelpers.ConvertToListOfType<Organization>(response);
+                var orgs = CloudApiHelpers.ConvertToListOfType<Organization>(response);
 
-            return Request.CreateResponse(HttpStatusCode.OK, orgs);
+                return Request.CreateResponse(HttpStatusCode.OK, orgs);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
         }
 
         [Route("api/SendCommand")]
         [HttpPost]
         public async Task<HttpResponseMessage> SendCommand(SourceDTO source)
         {
-            var response = await GenericApi.GenericRestfulClient.GetJson<object>(source.sourceName, source.command);
+            try
+            {
+                var response = await GenericApi.GenericRestfulClient.GetJson<object>(source.sourceName, source.command);
 
-            return Request.CreateResponse(HttpStatusCode.OK, response);
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
         }           
     }
 }
