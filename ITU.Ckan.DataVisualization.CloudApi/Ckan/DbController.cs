9c83ca9c7a050228669f69868c6ad5360edc353c
vitor_gz <dominguez.vitor@gmail.com>
2016-05-26 18:43:48 +0200

    Add DB controller to the rest API

---
+++ ITU.Ckan.DataVisualization.CloudApi/Ckan/DbController.cs
@@ -0,0 +1,32 @@
+﻿using ITU.Ckan.DataVisualization.InternalDslApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ITU.Ckan.DataVisualization.CloudApi.Ckan
+{
+    public class DbController : ApiController
+    {
+        [Route("api/GetVisualization")]
+        [HttpPost]
+        public HttpResponseMessage GetVisualization(string name)
+        {
+            var visual = DBClient.GetVisualizationByName(name);
+
+            return Request.CreateResponse(HttpStatusCode.OK, visual);
+        }
+
+        [Route("api/GetVisualizationList")]
+        public HttpResponseMessage GetVisualizationList()
+        {
+            var visuals = DBClient.GetListVisualizations();
+
+            return Request.CreateResponse(HttpStatusCode.OK, visuals);
+        }
+    }
+}
